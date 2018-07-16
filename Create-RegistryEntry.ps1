a9aa62afbb18843faf6d245c3ddde751fa65fb97
Slab Bulkchest <markwilkerson515@gmail.com>
2018-07-16 09:56:24 -0400

    Create registry entries with Powershell
    Quick and easy Powershell script that creates a registry entry.  Tailor it to your needs.

---
+++ Create-RegistryEntry.ps1
@@ -0,0 +1,20 @@
+﻿# Source: https://blogs.technet.microsoft.com/heyscriptingguy/2015/04/02/update-or-add-registry-key-value-with-powershell/
+# Change the values for your desired registry key as needed.
+# Please note this script creates a DWORD value.
+
+$registryPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\CredSSP\Parameters"
+
+$Name = "AllowEncryptionOracle"
+
+$value = "2"
+
+if (!(Test-Path $registryPath))
+    {
+        New-Item -Path $registryPath -Force | Out-Null
+        New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
+    }
+
+else 
+    {
+        New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
+    }
