21b4e64a712f96045cbde2477407ce42ad1d77fc
Slab Bulkchest <markwilkerson515@gmail.com>
2019-02-05 16:55:41 -0500

    Initial post

---
+++ Create-DepartmentDiskPaths.ps1
@@ -0,0 +1,15 @@
+﻿$rootmount = "c:\mount"
+$disktype = Get-Content .\disktype.txt
+$deptname = Get-Content .\departments.txt
+
+foreach ( $type in $disktype )
+    {     
+        foreach ( $deptshortname in $deptname )
+            {
+                $fullpath = $rootmount + "\" + "cinco_mail_" + $type + "_" + $deptshortname
+                
+                # Insert any verb logic here using the "fullpath" variable.
+                
+                md $fullpath
+            }
+    }
