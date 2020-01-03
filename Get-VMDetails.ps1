96a5acdaea13352f9eef35fcd3954b21d0534b85
Slab Bulkchest <41293910+markwilkerson@users.noreply.github.com>
2020-01-03 14:47:53 -0500

    Update Get-VMDetails.ps1

--- Get-VMDetails.ps1
+++ Get-VMDetails.ps1
@@ -7,4 +7,3 @@ foreach ( $line in $file )
         $replicationtag = Get-TagAssignment -Entity $line -Category "Veeam-Replication"
         Get-VM -Name $line | Select Name,@{N="Datastore";E={[string]::Join(',',(Get-Datastore -Id $_.DatastoreIdList | Select -ExpandProperty Name))}},@{N="Folder";E={$_.Folder.Name}},@{N="Replication Tag";E={$replicationtag.Tag}} | ft -AutoSize
     }
-    # hi
