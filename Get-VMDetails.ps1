0e876142ed8bace4d90ee43f5d94a1fec0b8087f
Slab Bulkchest <41293910+markwilkerson@users.noreply.github.com>
2020-01-03 14:47:00 -0500

    Checking VSCode git function

--- Get-VMDetails.ps1
+++ Get-VMDetails.ps1
@@ -1,9 +1,10 @@
-﻿# $file = Get-Content 'C:\users\mw148186\OneDrive - The Andersons, Inc\just_nothing.txt'
-$file = Import-Csv 'C:\users\mw148186\OneDrive - The Andersons, Inc\ExportList.csv'
+﻿$file = Get-Content 'C:\users\mw148186\OneDrive - The Andersons, Inc\just_nothing.txt'
+# $file = Import-Csv 'C:\users\mw148186\OneDrive - The Andersons, Inc\ExportList.csv'
 $output = 'C:\users\mw148186\OneDrive - The Andersons, Inc\theoutput.txt'
 
 foreach ( $line in $file )
     {
-        Get-VM -Name $line.Name | Select Name,@{N="Datastore";E={[string]::Join(',',(Get-Datastore -Id $_.DatastoreIdList | Select -ExpandProperty Name))}},@{N="Folder";E={$_.Folder.Name}} | ft -AutoSize | Out-File $output -Append
-        Get-TagAssignment -Entity $line.Name | ft -AutoSize | Out-File $output -Append
+        $replicationtag = Get-TagAssignment -Entity $line -Category "Veeam-Replication"
+        Get-VM -Name $line | Select Name,@{N="Datastore";E={[string]::Join(',',(Get-Datastore -Id $_.DatastoreIdList | Select -ExpandProperty Name))}},@{N="Folder";E={$_.Folder.Name}},@{N="Replication Tag";E={$replicationtag.Tag}} | ft -AutoSize
     }
+    # hi
